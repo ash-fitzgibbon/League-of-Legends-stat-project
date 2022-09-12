07db7412e652d0dafa79e3eb9ba001bf79361182
ash-fitzgibbon <107602601+ash-fitzgibbon@users.noreply.github.com>
2022-09-12 13:55:36 +1200

    fixed title

--- src/App.js
+++ src/App.js
@@ -34,7 +34,6 @@ function App() {
   return (
     <div className="App">
       <div className="container center">
-        <title>League Check</title>
         <h5>League of Legends Player Searcher</h5>
         <input
           type="text"
